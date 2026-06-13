cc7da7bd940c027d9db52bb68f3a65a6e95191a0
Manu-Castillo8 bits <ernesto.ccastilloguerra2@gmail.com>
2026-06-13 10:54:45 -0600

    first commit

---
+++ P7 Analisis05/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace P7_Analisis05
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ P7 Analisis05/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace P7_Analisis05
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

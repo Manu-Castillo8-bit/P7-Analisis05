89a8030215f8f5e92ad9a8f1b2a98352c0233f30
Manuel <ernesto.ccastilloguerra2@gmail.com>
2026-06-19 07:44:12 -0600

    Estructura terminada

--- P7 Analisis05/App.xaml.cs
+++ P7 Analisis05/App.xaml.cs
@@ -7,11 +7,7 @@ namespace P7_Analisis05
         public App()
         {
             InitializeComponent();
-        }
-
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
+            MainPage = new Poemas();
         }
     }
 }
